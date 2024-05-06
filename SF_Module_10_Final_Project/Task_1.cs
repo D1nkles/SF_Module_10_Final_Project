003ee76011c138ab33fadce35b6ca0e4ed088bdd
Ilia <isinetov@mail.ru>
2024-05-06 22:28:08 +0400

    Задание 1. Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. Определите интерфейс для функции сложения числа и реализуйте его.
    Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.

---
+++ SF_Module_10_Final_Project/Task_1.cs
@@ -0,0 +1,54 @@
+﻿namespace Task_1
+{
+    interface ISum
+    {
+        int Sum(int num1, int num2);
+    }
+
+    class Calculator : ISum
+    {
+        int Num1 { get; set; }
+        int Num2 { get; set; }
+        public Calculator()
+        {
+            while (true)
+            {
+                try
+                {
+                    Initialize();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: Вы ввели не число, либо число в неверном формате!!!\n" +
+                                      "Нажмите любую клавишу, чтобы попробовать снова...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public void Initialize() 
+        {
+            Console.Write("Введите первое целое число: ");
+            Num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе целое число: ");
+            Num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы сложить введенные числа...");
+            Console.ReadKey();
+            Console.WriteLine($"Сумма чисел равна {Sum(Num1, Num2)}"); 
+        }
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            new Calculator();
+        }
+    }
+}
